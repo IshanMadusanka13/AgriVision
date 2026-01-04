094a49be52011355a07be312f3bd1edcbc3b4313
shainiRose <shainiu22@gmail.com>
2026-01-04 19:43:53 +0530

    updated files

--- server/app/routes/disease_router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-from app.services.disease_service import process_uploaded_image
-
-router = APIRouter()
-
-@router.post("/")
-async def upload_image(file: UploadFile = File(...)):
-    result = await process_uploaded_image(file)
-    return result
